3fb837fcad38c935a015b61961189ab54cad34d9
GilbertoSavisky <developer.savisky@gmail.com>
2019-05-14 10:50:46 -0300

    Implementando os Itens de Menu - Pipe Currency

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import { ErrorHandler } from "app/app.error-handler";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -28,5 +29,11 @@ export class RestaurantsService {
             return this.http.get(`${MEAT_API}/restaurant/${id}/reviews`)
                 .map(response => response.json())
                 .catch(ErrorHandler.handleError)
+        }
+
+        menuOfRestaurant(id: string): Observable<MenuItem[]>{
+            return this.http.get(`${MEAT_API}/restaurant/${id}/menu`)
+                .map(response => response.json())
+                .catch(ErrorHandler.handleError)   
         }
 }
