9fa18bbcf875f4290e6c08b121524c9036e4575a
GILBERTO SAVISKY <gsavisky@AD.MPPR.MP.BR>
2019-09-25 16:15:01 -0300

    Navegação Programática via Router

--- src/app/order/order.services.ts
+++ src/app/order/order.services.ts
@@ -41,6 +41,7 @@ export class OrderService {
     return this.http.post(`${MEAT_API}/orders`,
         JSON.stringify(order),
         new RequestOptions({headers: headers}))
-      .map(response => response.json());
+      // tslint:disable-next-line:no-shadowed-variable
+      .map(response => response.json()).map(order => order.id);
   }
 }
