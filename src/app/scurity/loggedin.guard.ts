50cb3b1e6d81d6e34b7c907236a787f7d4a28717
GILBERTO SAVISKY <gsavisky@AD.MPPR.MP.BR>
2019-10-04 16:05:14 -0300

    121. Protegendo o Componente de Compra com CanActivate

--- src/app/scurity/loggedin.guard.ts
+++ src/app/scurity/loggedin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route } from '@angular/router';
+import { ActivatedRouteSnapshot, CanLoad, Route, RouterStateSnapshot } from '@angular/router';
 import { LoginService } from './login/login.service';
 
 @Injectable()
@@ -7,11 +7,19 @@ export class LoggedInGuard implements CanLoad {
 
   constructor(private loginService: LoginService) {}
 
-  canLoad(route: Route): boolean {
+  checkauthentication (path: string): boolean {
     const loggedIn = this.loginService.isloggedIn();
     if (!loggedIn) {
-      this.loginService.handleLogin(`/${route.path}`)
+      this.loginService.handleLogin(`/${path}`)
     }
     return loggedIn;
+  }
+
+  canLoad(route: Route): boolean {
+    return this.checkauthentication(route.path);
+  }
+
+  canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean {
+    return this.checkauthentication(activatedRoute.routeConfig.path);
   }
 }
